585b0d4777d83823969d88b8ef3300be0774d43a
createandbuild <33373263+createandbuild@users.noreply.github.com>
2020-11-03 05:45:16 -0500

    finished rest; need to flush bottom section

--- index.js
+++ index.js
@@ -7,9 +7,64 @@ sota.sotaConfig.sections = [
     // </div>
     // `
     // }
-    {"slug": "Election", "name": "Poll Results", "colors": sota.colorInterpolate("#434343")},
-    {"slug": "articles", "name": "Articles", "colors": sota.colorInterpolate("#000000"), "content":
+    {"slug": "Election", "name": "", "colors": sota.colorInterpolate("#663366")},
+    {"slug": "articles", "name": "", "colors": sota.colorInterpolate("#000000"), "content":
     `
+    <div class="article-container">
+      <h3 class="article-title">Andover Community Calls for Mutual Respect Ahead of Presidential Election</h3>
+      <h4 class="article-credit">By Erin Kim & Ananya Madduri</h4>
+      <p class="article-body">With less than two weeks until the 2020 Presidential Election, the Andover community is preparing for the November 3 contest between Republican nominee and current President Donald Trump and Democratic nominee and former
+        Vice President Joe Biden. In the days leading up to the election, community members are calling for unity and mutual respect across party lines.<br><br>
+
+        Student-run political organizations such as PA Democrats and the PA Republican Society (PARS) have hosted speaker events and election-themed club meetings. The two groups also plan on hosting joint meetings to foster civil discourse.<br><br>
+
+        On Wednesday, October 28, PARS will host Ramesh Ponnuru, a conservative “Bloomberg” columnist and “National Review” magazine senior editor, to speak on the future of American conservatism.<br><br>
+
+        PA Democrats hosted discussion-based meetings, including a conversation on what a conservative Supreme Court could mean for women’s reproductive rights, as well as a Biden-Harris phonebank organized by Mary Muromcew ’22.<br><br>
+
+        Many members of the Andover community have used these spaces to voice their opinions on the upcoming election. Corrie Martin, Instructor in English, and Aidan Pretti ’21 both called for the Andover community to refrain from hateful
+        rhetoric.<br><br>
+
+        “If I could give something like advice to young people as the election approaches, I want to say this: Reject hate. Reject bigotry. Reject sexism. Reject racism. Choose justice. Choose love. Choose your humanity,” wrote Martin in an email to
+        <i>The Phillipian</i>.<br><br>
+
+        “No matter who wins, I hope that election day (or rather, the days and weeks after it) can be a jumping-off point for everyone to tone down divisive rhetoric around campus and start to have more productive conversations about ideas, rather
+        than candidates,” wrote Pretti in an email to <i>The Phillipian</i>.<br><br>
+
+        Marcelle Doheny, Instructor in History and Social Science, encouraged others to vote.<br><br>
+
+        “The disturbing and dangerous rhetoric surrounding this year’s election, and the amount of disinformation, makes it very challenging for voters. Without saying which way I lean, my message is VOTE, VOTE, VOTE,” wrote Doheny in an email to
+        <i>The Phillipian</i>.<br><br>
+
+        Emma Jing ’22 noted that this year’s election will be unconventional with the Andover community scattered around the world. However, she is hopeful that Andover will be a space for open-minded and cohesive discussions across the
+        community.<br><br>
+
+        “I know how hard it is to connect with our peers right now, especially with half of the student body still remote, so it may not be the most realistic hope. However, I think that we still shouldn’t shy away from the topic of politics and,
+        although animosity runs rampant in our current political climate, maybe [Andover] can be a space where, when we encourage political engagement, we don’t require partisanship,” said Jing.<br><br>
+
+        Denise Taveras ’21 has noticed both an increasing number of political conversations and an enthusiasm among Seniors who are eligible to vote.<br><br>
+
+        “It’s really hard to have a space where people with very different ideas about the election can come together in informal settings, but I know that there is definitely a lot of conversation about what people want versus what people need from
+        our government. Especially with these last couple of years and Covid, a lot of people have very strong feelings about the upcoming election, and it really shows in some of the discussions,” said Taveras.</p>
+    </div>
+
+    <div class="commentary-container">
+      <h3 class="commentary-title">Commentary Counterpoint: The Electoral College</h3>
+      <span><a href="https://phillipian.net/2020/10/30/counterpoint-column-the-electoral-college/" class="read">Read More</a></span>
+      <p>Alex Mitchell ’22 and Sophie Glaser ’22 debate the importance of the electoral college, an issue that has been subject to much political discourse within the past few months.</p>
+      <article>
+        <p>According to Mitchell, “The authors of the Constitution sought to create a system in which every citizen’s vote mattered to those running for office: the electoral college accomplishes this in ways that a simple popular vote system never could. If only the popular vote mattered, urban citizens would control elections because of their far greater numbers. A system in which the citizens of our biggest cities controlled elections, where no one else’s votes mattered, would give urban voters a monopoly on political attention. Campaigns would likely never visit states with smaller populations, and thus those states’ interests would go ignored.”</p>
+      </article>
+      <article>
+        <p>Glaser wrote, “Democracy: a term closely associated with the United States, a word repeated in speeches by presidential candidates, critics, and politicians alike, a word that inspires ideas of liberty, freedom, and the American way. Federal elections are considered to be pillars of this idea of democracy, but they do not live up to this narrative. Our elections do not represent the people of the United States and the will of American citizens due to an outdated remnant of the past that serves only to impede representation in government: the electoral college.”</p>
+      </article>
+    </div>
+
+    <div class="commentary-img">
+      <img src="https://i1.wp.com/phillipian.net/wp-content/uploads/2020/10/Screen-Shot-2020-10-30-at-1.29.52-AM.png?w=1402&ssl=1">
+      <div class="image-credit"><span>Lauren Lee/<i>The Phillipian</i></span></div>
+    </div>
+
     <div class="articles-grid">
     <a class="article-item" href="https://phillipian.net/2020/10/22/andover-prepares-to-support-students-during-presidential-election/">
         <div class="article-category">
@@ -110,7 +165,7 @@ sota.sotaConfig.sections = [
         <p class ="author">
             Bianca Morales
         </p>
-      
+
     </a>
     <a class="article-item" href="https://phillipian.net/2020/10/08/donald-trumps-coronavirus-diagnosis/">
         <div class="article-category">
@@ -192,7 +247,6 @@ window.onload = () => {
         showLegend: true,
         title: "Are you eligible to vote in the U.S.?",
         subtitle: "",
-        customColors: ["#663366", "#855289"]
     })
 
     sota.stackedBarChart({
@@ -203,7 +257,6 @@ window.onload = () => {
         showLegend: true,
         title: "If you're eligible to vote in the U.S., are you registered to vote in the November 3 election?",
         subtitle:"",
-        customColors: ["#663366", "#855289"]
     })
 
     sota.stackedBarChart({
