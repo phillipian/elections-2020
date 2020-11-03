88bf472033d8bf740d230315e6e7df35206bbe76
createandbuild <33373263+createandbuild@users.noreply.github.com>
2020-11-03 02:14:42 -0500

    commiting changes bad practice meh

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 
 sota.sotaConfig.sections = [
-    // {"slug": "Intro", "name": "intro", "colors": sota.colorInterpolate("#336633"), "content":
+    // {"slug": "Intro", "name": "", "colors": sota.colorInterpolate("#336633"), "content":
     // `
-    // <div class="intro">
-
-    // <img src="/assets/election-banner.jpg"> 
-    // </div>
+    // <img src="/assets/" >
     // `
-    // }
     {"slug": "Election", "name": "Poll Results", "colors": sota.colorInterpolate("#663366")},
     {"slug": "articles", "name": "Articles", "colors": sota.colorInterpolate("#000000"), "content":
     `
+
+
     <div class="articles-grid">
     <a class="article-item" href="https://phillipian.net/2020/10/22/andover-prepares-to-support-students-during-presidential-election/">
         <div class="article-category">
-            <span> 
-                News 
+            <span>
+                News
             </span>
         </div>
         <h3>
             Andover Prepares to Support Students During Presidential Election
         </h3>
 
-        
+
 
         <p class="date">
-            October 22, 2020 
+            October 22, 2020
         </p>
         <p class ="author">
-            Erin Kim, Ananya Madduri 
+            Erin Kim, Ananya Madduri
         </p>
         <img src="https://i2.wp.com/phillipian.net/wp-content/uploads/2020/10/2016-election.png?w=1024&ssl=1">
     </a>
@@ -37,25 +35,25 @@ sota.sotaConfig.sections = [
 
     <a class="article-item" href="https://phillipian.net/2020/10/22/andover-community-calls-for-mutual-respect-ahead-of-presidential-election/">
         <div class="article-category">
-            <span> 
-                News 
+            <span>
+                News
             </span>
         </div>
         <h3>
             Andover Community Calls for Mutual Respect Ahead of Presidential Election
         </h3>
         <p class="date">
-            October 22, 2020 
+            October 22, 2020
         </p>
         <p class ="author">
-            Erin Kim, Ananya Madduri 
+            Erin Kim, Ananya Madduri
         </p>
     </a>
 
 
     <a class="article-item" href="https://phillipian.net/2020/10/01/students-react-to-first-presidential-debate/">
         <div class="article-category">
-            <span> 
+            <span>
                 News and Commentary
             </span>
         </div>
@@ -63,7 +61,7 @@ sota.sotaConfig.sections = [
             Students React to First Presidential Debate
         </h3>
         <p class="date">
-            October 1, 2020 
+            October 1, 2020
         </p>
         <p class ="author">
             Laura Ospina, Erin Kim, Hope Nardone
@@ -72,16 +70,16 @@ sota.sotaConfig.sections = [
 
     <a class="article-item" href="https://phillipian.net/2020/10/22/qanon-conspiracies-and-my-local-pizza-shop/">
         <div class="article-category">
-            <span> 
+            <span>
                 Commentary
             </span>
         </div>
         <h3>
             QAnon Conspiracies and My Local Pizza Shop
         </h3>
-        
+
         <p class="date">
-            October 22, 2020 
+            October 22, 2020
         </p>
         <p class ="author">
             Charlotte Rediker
@@ -91,53 +89,53 @@ sota.sotaConfig.sections = [
 
     <a class="article-item" href="https://phillipian.net/2020/10/23/battling-it-out-for-democracy/">
         <div class="article-category">
-            <span> 
+            <span>
                 Commentary
             </span>
         </div>
         <h3>
             Battling it out for Democracy
         </h3>
-        
+
         <p class="date">
-            October 23, 2020 
+            October 23, 2020
         </p>
         <p class ="author">
-            Bianca Morales 
+            Bianca Morales
         </p>
         <img src="https://i2.wp.com/phillipian.net/wp-content/uploads/2020/10/IMG_3247.jpg?resize=2048%2C1312&ssl=1">
     </a>
 
     <a class="article-item" href="https://phillipian.net/2020/10/08/donald-trumps-coronavirus-diagnosis/">
         <div class="article-category">
-            <span> 
+            <span>
                 Commentary
             </span>
         </div>
         <h3>
             Donald Trump's Coronavirus Diagnosis
         </h3>
-        
+
         <p class="date">
-            October 8, 2020 
+            October 8, 2020
         </p>
         <p class ="author">
-            Bianca Morales 
+            Bianca Morales
         </p>
         <img src="https://i1.wp.com/phillipian.net/wp-content/uploads/2020/10/Image-from-iOS-3.jpg?resize=2048%2C1508&ssl=1">
     </a>
 
     <a class="article-item" href="https://phillipian.net/2020/10/08/donald-trumps-coronavirus-diagnosis/">
         <div class="article-category">
-            <span> 
+            <span>
                 Editorial
             </span>
         </div>
         <h3>
-            Deconstructing Disinformation 
+            Deconstructing Disinformation
         </h3>
         <p class="date">
-            October 11, 2020 
+            October 11, 2020
         </p>
         <p class ="author">
             The Phillipian, vol. CXLIII
@@ -146,7 +144,7 @@ sota.sotaConfig.sections = [
 
     <a class="article-item" href="https://phillipian.net/2020/09/25/honoring-justice-ruth-bader-ginsburg-1933-2020/">
         <div class="article-category">
-            <span> 
+            <span>
                 Editorial
             </span>
         </div>
@@ -154,16 +152,16 @@ sota.sotaConfig.sections = [
             Honoring Justice Ruth Bader Ginsburg (1933-2020)
         </h3>
         <p class="date">
-            September 25, 2020 
+            September 25, 2020
         </p>
         <p class ="author">
             The Phillipian, vol. CXLIII
         </p>
     </a>
-    
+
     <a class="article-item" href="https://phillipian.net/2020/09/25/honoring-justice-ruth-bader-ginsburg-1933-2020/">
     <div class="article-category">
-        <span> 
+        <span>
             Editorial
         </span>
     </div>
@@ -171,7 +169,7 @@ sota.sotaConfig.sections = [
         Honoring Justice Ruth Bader Ginsburg (1933-2020)
     </h3>
     <p class="date">
-        September 25, 2020 
+        September 25, 2020
     </p>
     <p class ="author">
         The Phillipian, vol. CXLIII
@@ -181,7 +179,7 @@ sota.sotaConfig.sections = [
 
 <a class="article-item" href="https://www.youtube.com/embed/AvBodTW7WU8">
     <div class="article-category">
-        <span> 
+        <span>
             Editorial
         </span>
     </div>
@@ -189,7 +187,7 @@ sota.sotaConfig.sections = [
         Honoring Justice Ruth Bader Ginsburg (1933-2020)
     </h3>
     <p class="date">
-        September 25, 2020 
+        September 25, 2020
     </p>
     <p class ="author">
         The Phillipian, vol. CXLIII
@@ -215,7 +213,7 @@ sota.createSections(sota.sotaConfig);
 
 // optional, render navbar based on sotaConfig.sections
 // make sure you call createSections first!
-sota.sotaNavbar(sota.sotaConfig, "Mock Election 2020", "assets/img/p-square.png", false, "https://www.phillipian.net/");
+sota.sotaNavbar(sota.sotaConfig, "sota.js Demo", "szlogo.png", false, "https://www.samsonzhang.com/");
 // GENERAL
 window.onload = () => {
 
